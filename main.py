c69a54902c45e3cd8d8d94f0d290b63eb33e82d0
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-06 14:37:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
 
 
-for index, row in df.iterrows():
-    pdf.add_page()
+num = row["Pages"]
+    for i in range(num):
+        pdf.add_page()
 
-    # any cell after this line will be the following style
-    pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(90, 0, 50)
-    # ln=1 is a break line. if ln=0, there is no break and the cells with collide
-    # align=L is left
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-    pdf.line(10, 21, 200, 21)
+        # any cell after this line will be the following style
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.set_text_color(90, 0, 50)
+        # ln=1 is a break line. if ln=0, there is no break and the cells with collide
+        # align=L is left
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+        pdf.line(10, 21, 200, 21)
 
 pdf.output("output.pdf")
